4250b94c88d8864bc5e6f0b2945c4ffcff8a2cab
avinash <avinashearla95@gmail.com>
2025-11-05 01:09:28 +0530

    added changes to the api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
+// Automatically switch between local and production API
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  withCredentials: true, 
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://127.0.0.1:8000" // 👈 Local backend (development)
+      : "https://expense-tracker-fastapi-s222.onrender.com", // 👈 Deployed backend (Render)
+  withCredentials: false,
 });
 
+// Attach Authorization header if token exists
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
